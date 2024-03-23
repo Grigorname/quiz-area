f9eeab2ec844859f8b6139b0a2fe05d83ab50a85
Dav991123 <sargsyand89@gmail.com>
2024-03-23 18:38:15 +0400

    change quiz step

--- script/db.js
+++ script/db.js
@@ -8,17 +8,99 @@ export const questions = [
             'hello',
             'World Hello'
         ],
-        correctOption: 1
+      correctOption: 1  
     },
 
     {
         quizTitle: 'Ի՞նչ կտպվի console-ում 2',
         quiz: 'console.log(Hello World)',
         options: [
-            'world',
-            'Hello World',
+            'world(2 quiz)',
+            'Hello World (2 quiz)',
         ],
-        correctOption: 1
+        correctOption: 0
+    },
+
+
+    {
+        quizTitle: 'Ի՞նչ կտպվի console-ում 3',
+        quiz: 'console.log(Hello World)',
+        options: [
+            'world(3 quiz)',
+            'Hello World (3 quiz)',
+            'Hello World (3 quiz)',
+            'Hello World (3 quiz)',
+            'Hello World (3 quiz)',
+        ],
+        correctOption: 3
+    },
+
+    {
+        quizTitle: 'Ի՞նչ կտպվի console-ում 4',
+        quiz: 'console.log(Hello World)',
+        options: [
+            'world(4 quiz)',
+            'Hello World (4 quiz)',
+        ],
+        correctOption: 0
+    },
+
+    {
+        quizTitle: 'Ի՞նչ կտպվի console-ում 4',
+        quiz: 'console.log(Hello World)',
+        options: [
+            'world(4 quiz)',
+            'Hello World (4 quiz)',
+        ],
+        correctOption: 0
+    },    {
+        quizTitle: 'Ի՞նչ կտպվի console-ում 4',
+        quiz: 'console.log(Hello World)',
+        options: [
+            'world(4 quiz)',
+            'Hello World (4 quiz)',
+        ],
+        correctOption: 0
+    },    {
+        quizTitle: 'Ի՞նչ կտպվի console-ում 4',
+        quiz: 'console.log(Hello World)',
+        options: [
+            'world(4 quiz)',
+            'Hello World (4 quiz)',
+        ],
+        correctOption: 0
+    },    {
+        quizTitle: 'Ի՞նչ կտպվի console-ում 4',
+        quiz: 'console.log(Hello World)',
+        options: [
+            'world(4 quiz)',
+            'Hello World (4 quiz)',
+        ],
+        correctOption: 0
+    },    {
+        quizTitle: 'Ի՞նչ կտպվի console-ում 4',
+        quiz: 'console.log(Hello World)',
+        options: [
+            'world(4 quiz)',
+            'Hello World (4 quiz)',
+        ],
+        correctOption: 0
+    },    {
+        quizTitle: 'Ի՞նչ կտպվի console-ում 4',
+        quiz: 'console.log(Hello World)',
+        options: [
+            'world(4 quiz)',
+            'Hello World (4 quiz)',
+        ],
+        correctOption: 0
+    },    {
+        quizTitle: 'Ի՞նչ կտպվի console-ում 6',
+        quiz: 'console.log(Hello World)',
+        options: [
+            'world(4 quiz)',
+            'Hello World (4 quiz)',
+        ],
+        correctOption: 0
     },
 ]
 
--- script/index.js
+++ script/index.js
@@ -1,6 +1,6 @@
 import { defaultLanguage, translates } from './constnats.js';
 import { valueSetter, tagCreator } from './helpers.js';
-import { questions } from './db.js'; //harcer
+import { questions } from './db.js'; 
 
 const speed = 50;
 let i = 0;
@@ -15,6 +15,9 @@ const typeWriter = () => {
         i++;
         setTimeout(typeWriter, speed);
     } else {
+        warningContainer.style.display = 'none';
+        document.getElementById('quizSection').style.transform = 'translate(0, 50px)';
+
     }
 
 };
@@ -27,19 +30,65 @@ typeWriter();
 // quiz code area
 
 const quizGenerator = (quizList) => {
-    let quizStep = 0;
-    const question = quizList[quizStep];//mek harcy
-    console.log(question);
-    return () => {
+    let quizStep = 0; //step
+    
+    console.log(quizStep, 'top');
+    const quizCount = quizList.length;
+    let correctAnswer = 0;
+    let percent = 0;
+    const ulContainer = document.getElementById('quiz_options');
+
+    const nextStep = (position) => {
+        if (position === 'next' && quizStep < quizCount) {
+            quizStep++;
+        } else if (position === 'prev' && quizStep > 0) {
+            quizStep--;
+        }
+        const question = quizList[quizStep];  //0[0]
+        // 
+
+        console.log(quizStep, 'quizStep');
+        
+        ulContainer.innerText = '';
+
         valueSetter('#quiz_title', question.quizTitle);
         valueSetter('#helpers', question.quiz);
 
-        question.options.forEach((quiz) => {
-            tagCreator('li', document.getElementById('quiz_options'), quiz)
+   
+        question.options.forEach((quiz, index) => {
+            tagCreator('li', ulContainer, quiz, index)
+        });
+
+
+        ulContainer.addEventListener('click', (e)=> {
+            if (e.target.tagName === 'LI') {  
+                if (e.target.dataset.option == question.correctOption) {
+                    correctAnswer++;
+                }
+            } 
         });
     }
+
+    return nextStep;
 };
 
 const quizConfig =  quizGenerator(questions); //quiz 1 berec
 
 quizConfig();
+
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+
+prev.addEventListener('click', () => {
+    quizConfig('prev');
+})
+
+next.addEventListener('click', () => {
+    quizConfig('next');
+})
+
+
+// console.log(document.querySelectorAll('.button_conatiner > button'))
+
+
+
